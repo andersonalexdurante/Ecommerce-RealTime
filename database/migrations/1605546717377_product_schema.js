9ac20079d8702d2684fa4dc56e8090e0cfd1c8a9
andersonalexdurante <anderson.alexdurante@gmail.com>
2020-11-16 14:23:31 -0300

    Corrige relacionamentos de produtos

--- database/migrations/1605546717377_product_schema.js
+++ database/migrations/1605546717377_product_schema.js
@@ -15,17 +15,26 @@ class ProductSchema extends Schema {
       table.timestamps()
     })
 
-    this.create('images_product', table => {
+    this.create('image_product', table => {
       table.increments()
       table.integer('image_id').unsigned()
       table.integer('product_id').unsigned()
       table.foreign('image_id').references('id').inTable('images').onDelete('cascade').onUpdate('cascade')
       table.foreign('product_id').references('id').inTable('products').onDelete('cascade').onUpdate('cascade')
     })
+
+    this.create('category_product', table => {
+      table.increments()
+      table.integer('category_id').unsigned()
+      table.integer('product_id').unsigned()
+      table.foreign('category_id').references('id').inTable('categories').onDelete('cascade').onUpdate('cascade')
+      table.foreign('product_id').references('id').inTable('products').onDelete('cascade').onUpdate('cascade')
+    })
   }
 
   down () {
-    this.drop('images_product')
+    this.drop('category_product')
+    this.drop('image_product')
     this.drop('products')
   }
 }
