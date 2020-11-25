ff4385547d216d5ed9a19cdf59d4d96ac82620c2
andersonalexdurante <anderson.alexdurante@gmail.com>
2020-11-25 17:56:50 -0300

    Cria métodos de login, cadastro, reset token e logout de usuários

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -1,13 +1,61 @@
 'use strict'
 
+const Database = use('Database')
+const User = use('App/Models/User')
+const Role = use('Role')
+
 class AuthController {
-  async register({ request, response }) {}
+  async register({ request, response }) {
+    const trx = await Database.beginTransaction()
+    try {
+      const { name, surname, email, password } = request.all()
 
-  async login({ request, response, auth }) {}
+      const user = await User.create({ name, surname, email, password }, trx)
+      const userRole = await Role.findBy('slug', 'client')
+      await user.roles().attach([userRole.id], null, trx)
+      await trx.commit()
 
-  async refreshToken({ request, response, auth }) {}
+      return response.status(201).send({ data: user })
+    } catch (error) {
+      await trx.rollback()
+      return response
+        .status(400)
+        .send({ message: 'Erro ao realizar o cadastro' })
+    }
+  }
 
-  async logout({ request, response, auth }) {}
+  async login({ request, response, auth }) {
+    const { email, password } = request.all()
+
+    let data = await auth.withRefreshToken().attempt(email, password)
+    return response.send({ data })
+  }
+
+  async refreshToken({ request, response, auth }) {
+    var refreshToken = request.input('refresh_token')
+
+    if (!refreshToken) {
+      refreshToken = request.header('refresh_token')
+    }
+
+    const user = await auth
+      .newRefreshToken()
+      .generateForRefreshToken(refreshToken)
+
+    return response.send({ data: user })
+  }
+
+  async logout({ request, response, auth }) {
+    var refreshToken = request.input('refresh_token')
+
+    if (!refreshToken) {
+      refreshToken = request.header('refresh_token')
+    }
+
+    await auth.authenticator('jwt').revokeTokens([refreshToken], true)
+
+    return response.status(204).send({})
+  }
 
   async forgot({ request, response }) {}
 
